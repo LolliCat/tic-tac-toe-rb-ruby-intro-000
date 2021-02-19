149509673cdfb49af9f4c6909d97a350717b41b5
Tedi <tmlowney1@gmail.com>
2021-02-19 19:54:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,7 +10,16 @@ WIN_COMBINATIONS = [
   [2,5,8]
 ]
 
-
+# Define display_board that accepts a board and prints
+# out the current state.
+#board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
 
 # rspec spec/01_tic_tac_toe_spec.rb
